8082444d9e80ee334ab62536b9f0839f7f9fc7af
navin <tanzimndub@gmail.com>
2016-12-25 21:32:03 +0600

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,23 @@
+package github.pkg3;
+
+import java.util.Scanner;
+
+public class NewClass {
+
+    public static void main(String[] args) {
+        int a, b, c;
+
+        Scanner scan = new Scanner(System.in);
+        a = scan.nextInt();
+
+        b = scan.nextInt();
+
+        c = scan.nextInt();
+        
+         System.out.println("Day:" + a);
+          System.out.println("Month:" + b);
+           System.out.println("Year:" +c);
+        
+    }
+
+}
